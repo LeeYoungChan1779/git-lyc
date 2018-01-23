4fc935c6f817c82eb412dd6010300766f4f308d7
DESKTOP-KLHQH2Q\LCT <dldudcks159@gmail.com>
2018-01-23 13:38:55 +0900

    Scanner로 입력 및 입력 값 출력

--- src/GitProjPractice/gitProjPractice.java
+++ src/GitProjPractice/gitProjPractice.java
@@ -1,7 +1,19 @@
 package GitProjPractice;
 
+import java.util.Scanner;
+
 public class gitProjPractice {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("안녕?");
+		
+		// 입력
+		System.out.println("숫자를 입력하세요.");
+		int a = sc.nextInt();
+		System.out.println("숫자를 재입력하세요.");
+		int b = sc.nextInt();
+		
+		// 출력
+		System.out.println("첫 번째 숫자 : " + a + "   두 번째 숫자 : " + b);
 	}
 }
